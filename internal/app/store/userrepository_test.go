d19720b8f5171909479aca350cd9942ab348102d
ajiways <ajiwayswc@gmail.com>
2023-04-20 05:51:32 +0700

    Part 3 (Working with validation)

--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -11,9 +11,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
@@ -23,13 +21,18 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	defer teardown("users")
 
 	email := "user@example.org"
-
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u := model.TestUser(t)
+	u.Email = email
+
+	_, err = s.User().Create(u)
+	if err != nil {
+		t.Error(err)
+	}
+
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
